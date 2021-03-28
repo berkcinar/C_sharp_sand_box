cf5fdf30d5df82c1bde2203b2bd7a1a36c1ba789
berkcinar <berkcinar01@gmail.com>
2021-03-28 11:46:28 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace new_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string charName = "Berk" ;
+            int charAge ;
+            charAge=35;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!2");
+            Console.WriteLine(charName);
+            Console.WriteLine(charAge);
+        }
+    }
+}
